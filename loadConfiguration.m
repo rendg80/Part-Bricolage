4535a5e83b962198bd1b80c45977efc32ba38277
Sukrit Shankar <sukritshankar@users.noreply.github.com>
2014-10-14 05:31:58 +0100

    Update loadConfiguration.m

--- loadConfiguration.m
+++ loadConfiguration.m
@@ -11,10 +11,13 @@ function configPB = loadConfiguration()
 % and in general, one finds it easier to run in parts, store the results,
 % and use this storage in the subsequent parts. 
 % The params are listed in order that they need to be run in the code. 
-configPB.runPartDetectors = 1; 
-configPB.runFlow = 1; 
-configPB.resolvePartDetectionAmbiguity = 1; 
+configPB.runPartDetectors = 0; 
+configPB.runFlow = 0; 
+configPB.resolvePartDetectionAmbiguity = 0; 
 configPB.utilizeFlow = 1; 
+configPB.doTraining = 0;
+configPB.formGraphs = 0;
+configPB.doDetection = 0;  % Includes Classification  
 
 % Load the config param for the dataset - Never start from zero 
 % 1 - Hollywood (for activity detection)
@@ -31,7 +34,8 @@ configPB.frameSeparation = [20, 6]; % For datasets in order and according to the
 % might be very expensive for large datasets (like the ones we have).
 % For tracking using detection on sparse parts to make perfect sense, make
 % sure that frameSeparationForFlow wholly divides frameSeparation
-configPB.frameSeparationForFlow = [5, 2]; % For datasets in order and according to the type of videos in the dataset 
+configPB.frameSeparationForFlow = [5, 2]; % For datasets in order and according to the type of videos in the dataset
+configPB.normalizeFlow = 0;  % Keep it zero unless you want to experiment 
 
 % Load the config param for the minimum no of torsos detected with poselets
 % Can change dependeing on the number of people generally found in the
@@ -45,7 +49,17 @@ configPB.considerOnlyFMP = 0; % 1 = consider only FMP (not poselets and torsos)
 % Load the configuration variable which decides whether to use flow or not
 configPB.useFlowInformation = 1; 
 
-% Load the configuration variable for the histogram binsize 
+% Load the configuration variable for type of tracking 
+% 0 = Max Frequency Flow ; 1 = Median
+% Note - For tracking in our case, entire part needs a fixed flow for
+% avoiding distortions in shape.
+configPB.typeOfTracking = 0; 
+
+% Load the configuration variable for the histogram binsize - For Flow
 configPB.histBinSize = 10; 
+configPB.histWindowSizeSpatialPercentage = 0.2; 
+configPB.histWindowSizeTemporalPixels = 1; 
+configPB.binNumberOfFlowAngles = 20;
+configPB.thresholdForFlowConsideration = 0.1; 
 
 
